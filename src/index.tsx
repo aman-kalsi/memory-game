e99c5d7820d0a4df1b9cf63cdb27f81d5d88badd
amank98 <amank98@gmail.com>
2021-11-28 21:04:15 -0500

    snow background

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Snowfall from 'react-snowfall'
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <Snowfall />
+    <App />
+  </div>,
   document.getElementById('root')
 );
